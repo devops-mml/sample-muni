82d1f535ca9419f840e0e550d4120f6facea82d1
Muneswar-4256 <muneswar42@gmail.com>
2022-12-22 16:52:20 +0530

    sample project

---
+++ sample/src/test/java/com/lti/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
